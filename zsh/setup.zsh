78da79ed48a404393bd8a928dff0cd6dfc4ae550
Maxime Peschard <maxime.peschard@gmail.com>
2016-06-25 21:30:04 +0200

    Zsh : add PDF compression function

--- zsh/setup.zsh
+++ zsh/setup.zsh
@@ -77,3 +77,32 @@ function makeslides () {
     makepdf $filename && rm -f $prefix.nav $prefix.snm $prefix.vrb
 }
 
+# Reduce the size of a PDF file
+# Usage : pdfopt file.pdf
+# (from : http://tex.stackexchange.com/q/18987)
+function pdfopt () {
+    file=$1
+    filebase=$(basename $file .pdf)
+    optfile=/tmp/$$-${filebase}_opt.pdf
+    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH \
+            -sOutputFile=${optfile} ${file}
+    if [ $? == '0' ]; then
+        optsize=$(stat -c "%s" ${optfile})
+        orgsize=$(stat -c "%s" ${file})
+        if [ "${optsize}" -eq 0 ]; then
+            echo "No output!  Keeping original"
+            rm -f ${optfile}
+            exit;
+        fi
+        if [ ${optsize} -ge ${orgsize} ]; then
+            echo "Didn't make it smaller! Keeping original"
+            rm -f ${optfile}
+            exit;
+        fi
+        bytesSaved=$(expr $orgsize - $optsize)
+        percent=$(expr $optsize '*' 100 / $orgsize)
+        echo Saving $bytesSaved bytes \(now ${percent}% of old\)
+        rm ${file}
+        mv ${optfile} ${file}
+    fi
+}
