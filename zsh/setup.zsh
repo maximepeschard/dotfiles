e537d5fe9a0b91c86347d44a2cfd94c9e7be19e1
Maxime Peschard <maxime.peschard@gmail.com>
2017-02-18 16:16:24 +0100

    Cleanup old functions

--- zsh/setup.zsh
+++ zsh/setup.zsh
@@ -34,6 +34,7 @@ alias ..="cd .."
 alias ...="cd ../.."
 alias .....="cd ../../.."
 alias grep="grep --color=auto"
+alias tree="tree -FC"
 alias ga="git add"
 alias gaa="git add --all"
 alias gc="git commit -v"
@@ -42,10 +43,8 @@ alias gl="git log"
 alias glp="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias gls="glp --stat"
 alias grs="git remote show"
-alias LS="displaywarning 'TURN THAT CAPS LOCK OFF, SIR.' && ls"
-alias LA="displaywarning 'TURN THAT CAPS LOCK OFF, SIR.' && la"
-alias LL="displaywarning 'TURN THAT CAPS LOCK OFF, SIR.' && ll"
-alias LLA="displaywarning 'TURN THAT CAPS LOCK OFF, SIR.' && lla"
+alias gnoprompt="touch .git_no_prompt"
+alias gclean="git stash -u && git stash drop" # discard all local changes on both tracked/untracked
 
 #### RUBY ####
 # Rbenv for Ruby
@@ -57,69 +56,8 @@ if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
 
 #### UTILITIES ####
 
-function displaywarning () {
-    RED='\033[0;31m'
-    ORANGE='\033[0;33m'
-    NC='\033[0m'
-    TEXT="$@"
-    echo -e "${RED}[!]${NC}\n${RED}[!]${NC} ${ORANGE}${TEXT}${NC}\n${RED}[!]${NC}"
-}
-
 # Create and enter a directory
 function mkcd () {
     mkdir "$@" && cd "$@"
 }
 
-# Tex to pdf and remove aux files
-function makepdf () {
-    filename="$1"
-    prefix=${filename%.tex}
-    pdflatex $filename && pdflatex $filename && rm -f $prefix.aux $prefix.log $prefix.out $prefix.toc
-}
-
-# TeX to pdf with bibliography
-function pdfbib () {
-    filename="$1"
-    prefix=${filename%.tex}
-    pdflatex $filename &&
-    bibtex $prefix &&
-    makepdf $filename &&
-    rm -f $prefix.bbl $prefix.blg
-}
-
-# Beamer to pdf and remove aux files
-function makeslides () {
-    filename="$1"
-    prefix=${filename%.tex}
-    makepdf $filename && rm -f $prefix.nav $prefix.snm $prefix.vrb
-}
-
-# Reduce the size of a PDF file
-# Usage : pdfopt file.pdf
-# (from : http://tex.stackexchange.com/q/18987)
-function pdfopt () {
-    file=$1
-    filebase=$(basename $file .pdf)
-    optfile=/tmp/$$-${filebase}_opt.pdf
-    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH \
-            -sOutputFile=${optfile} ${file}
-    if [ $? == '0' ]; then
-        optsize=$(stat -c "%s" ${optfile})
-        orgsize=$(stat -c "%s" ${file})
-        if [ "${optsize}" -eq 0 ]; then
-            echo "No output!  Keeping original"
-            rm -f ${optfile}
-            exit;
-        fi
-        if [ ${optsize} -ge ${orgsize} ]; then
-            echo "Didn't make it smaller! Keeping original"
-            rm -f ${optfile}
-            exit;
-        fi
-        bytesSaved=$(expr $orgsize - $optsize)
-        percent=$(expr $optsize '*' 100 / $orgsize)
-        echo Saving $bytesSaved bytes \(now ${percent}% of old\)
-        rm ${file}
-        mv ${optfile} ${file}
-    fi
-}
